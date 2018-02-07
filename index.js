8edbd639ed930c27a54d3770354b4d22a728ea38
ttaylorxv <35540721+ttaylorxv@users.noreply.github.com>
2018-02-06 22:32:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', (req, res) => res.send('Hello New Master #2 World!'))
+app.get('/', (req, res) => res.send('Hello New Master #3 World!'))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
